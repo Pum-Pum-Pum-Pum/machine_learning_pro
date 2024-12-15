250113a7ef4106eb7dbcd825dba9a37c62d05e8e
Utta Pum <pvnkmrwrlrd@gmail.com>
2024-12-15 11:47:02 +0800

    model predicted

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -13,6 +13,7 @@ from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from xgboost import XGBRFRegressor
 from sklearn.tree import DecisionTreeRegressor
+#from catboost import CatBoostRegressor
 
 from src.exception import CustomException
 from src.logger import logging
@@ -46,35 +47,73 @@ class ModelTrainer:
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "K-nearest Neighbour Regressor": KNeighborsRegressor(),
-                "XGRegressor": XGBRFRegressor(),
-                #"CatBoosting Regressor":CatBoostRegressor(),
+                "XGBRegressor": XGBRFRegressor(),
+                #"CatBoosting Regressor":CatBoostRegressor(verbose=False),
                 "Adaboost regressor": AdaBoostRegressor()
             }
             
-            model_report:dict = evaluate_model(X_train=X_train, y_train=y_train, X_test= X_test, y_test=y_test, models = models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "K-nearest Neighbour Regressor":{"n_neighbors": [3, 5, 7], "weights": ["uniform", "distance"]},
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "Adaboost regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+            
+            model_report, best_models  = evaluate_model(X_train=X_train, y_train=y_train, X_test= X_test, y_test=y_test, models = models,param=params)
             
             ## Getting best Model score and best model name
-            
-            best_model_score = max(sorted(model_report.values()))
-            best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
-            ]
-            
-            best_model = models[best_model_name]
-            
+            # Get the best model based on the highest score
+            best_model_score = max(model_report.values())
+            best_model_name = max(model_report, key=model_report.get)
+            best_model = best_models[best_model_name]
+                
             if best_model_score < 0.6:
-                raise CustomException("All models are not good enough")
+                raise CustomException("No suitable model found with acceptable performance.")
             else:
-                logging.info(f"Best model found {best_model}")
-            
+                logging.info(f"Best model: {best_model_name} with r2_score: {best_model_score}")
+
+            # Save the best fitted model
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
             )
-            
-            predicted=best_model.predict(X_test)
-            
-            r2_score_value = r2_score(y_test, predicted)
+
+            # Make predictions using the best model
+            predictions = best_model.predict(X_test)
+
+            # Calculate and return the r2 score for the best model
+            r2_score_value = r2_score(y_test, predictions)
             return r2_score_value
         
         except Exception as e:
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -16,21 +17,34 @@ def save_object(file_path, obj):
     except Exception as e:
         CustomException(e,sys)
         
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models,param):
     try:
         report = {}
+        best_models = {}
         
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        for i, model_name in enumerate(models.keys()):
+            model = models[model_name]
+            para = param.get(model_name, {})  # Get parameter grid for the model
             
-            model.fit(X_train, y_train)
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+            # Perform GridSearchCV
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
             
+            # Get the best estimator from GridSearchCV
+            best_model = gs.best_estimator_
+            
+            # Make predictions using the best model
+            y_train_pred = best_model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
+            
+            # Calculate r2 scores
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
             
-            report[list(models.keys())[i]] = test_model_score
-        return report
+            # Store the test score and the fitted model
+            report[model_name] = test_model_score
+            best_models[model_name] = best_model
+
+        return report, best_models  # Return both scores and fitted models
     except Exception as e:
         CustomException(e, sys)
